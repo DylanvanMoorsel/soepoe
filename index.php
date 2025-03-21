a60b0c8511b8a353f62627feef9f503b4f1eccba
DylanvanMoorsel <1206261@student.roc-nijmegen.nl>
2025-03-21 16:17:08 +0100

    Added a search option

--- index.php
+++ index.php
@@ -8,11 +8,19 @@
 </head>
 
 <body>
-    <section class="select-menu">
+    <div class="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="#about">Contact</a>
+        <a href="#contact">Admin</a>
+        <input type="text" placeholder="Search..">
+    </div>
 
+    <section class="select-menu">
         <div class="soepoe">
             <img src="assets/img/soep-header.png" alt="soeplogo" width="100%" height="90%">
         </div>
     </section>
+
 </body>
+
 </html
